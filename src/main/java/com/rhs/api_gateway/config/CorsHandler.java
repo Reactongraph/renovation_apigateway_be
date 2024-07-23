158249f80e46b06cc1478a5ee5559d4ef9c138c7
Amitsinghongraph <amit.chauhan@ongraph.com>
2024-07-23 23:25:21 +0530

    setup api gateway

---
+++ src/main/java/com/rhs/api_gateway/config/CorsHandler.java
@@ -0,0 +1,26 @@
+package com.tripply.tripplyapigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsHandler {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3721"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfig.addAllowedHeader("*");
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+
+}
